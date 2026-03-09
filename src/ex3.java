9e4f163277f41288168935754ec409bf2d6fb000
ilyakutarba-source <ilyakutarba@gmail.com>
2026-03-09 19:36:40 +0300

    lesson2/ex3

---
+++ src/ex3.java
@@ -0,0 +1,13 @@
+/**
+ * @author Ilya
+ * @date 08.03.2026
+ */
+//Задача №3. Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно
+public class ex3 {
+    public static void main(String[] args) {
+        System.out.print("Квадраты чисел от 10 до 20: ");
+        for (int i = 10; i <= 20; i++) {
+            System.out.print(i * i + " ");
+        }
+    }
+}
