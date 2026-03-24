0a69618f767c22cb103faf2986624af002058679
ilyakutarba-source <ilyakutarba@gmail.com>
2026-03-24 17:48:36 +0300

    lesson7 HomeWork 7 Ex1Worker

---
+++ src/Worker.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ilya
+ * @date 24.03.2026
+ */
+
+public class Worker implements Employee{
+    @Override
+    public void printPosition() {
+        System.out.println("Рабочий");
+    }
+}
