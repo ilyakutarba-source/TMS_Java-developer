349bea09114fe083f5e7eb3ec275f284fd68b067
ilyakutarba-source <ilyakutarba@gmail.com>
2026-03-29 02:12:30 +0300

    lesson 8 HomeWork 8 Ex1Dentist

---
+++ src/Dentist.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ilya
+ * @date 29.03.2026
+ */
+
+public class Dentist extends Doctor {
+    @Override
+    public void treat() {
+        System.out.println("Дантист лечит зубы");
+    }
+}
